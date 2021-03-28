1ade65bf67830409ac50502dcd6abbae724a3e44
AzureAD\AlexisCampan <alexis.campan@toulouse.miage.fr>
2021-03-29 01:18:07 +0200

    first card

--- mousseinfo_mob/src/app/tab1/tab1.page.ts
+++ mousseinfo_mob/src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as Leaflet from 'leaflet';
+import { antPath } from 'leaflet-ant-path';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,74 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  map: Leaflet.Map;
+  propertyList = [];
+  lat : number;
+  long : number;
+
+  constructor() {
+    this.lat=0;
+  }
+
+  ngOnInit(): void {
+
+
+
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.lat = position.coords.latitude;
+      this.long = position.coords.longitude;
+    });
+
+    // navigator.geolocation.getCurrentPosition(success, error, options);
+  }
+
+  ionViewDidEnter() {
+    console.log(this.lat);
+    console.log(typeof this.lat);
+    console.log(typeof 43.5781632);
+
+
+
+    this.map = new Leaflet.Map('map').setView([this.lat , this.long], 20);
+
+    Leaflet.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+      attribution: 'edupala.com'
+    }).addTo(this.map);
+
+    var LeafIcon = Leaflet.Icon.extend({
+      options: {
+         iconSize:     [50, 50],
+         iconAnchor:   [22, 94],
+         popupAnchor:  [-3, -76]
+      }
+  });
+
+    var greenIcon = new LeafIcon({
+      iconUrl: 'assets/location.png',
+  })
+
+    Leaflet.marker([this.lat, this.long], {icon: greenIcon}).addTo(this.map);
+
+    fetch('./assets/data.json')
+      .then(res => res.json())
+      .then(data => {
+        this.propertyList = data.properties;
+        this.map;
+      })
+      .catch(err => console.error(err));
+  }
+
+  leafletMap() {
+    for (const property of this.propertyList) {
+      Leaflet.marker([property.lat, property.long]).addTo(this.map)
+        .bindPopup(property.city)
+        .openPopup();
+    }
+  }
+
+  ionViewWillLeave() {
+    this.map.remove();
+  }
+
 
 }
--- mousseinfo_mob/src/app/tab2/tab2.page.ts
+++ mousseinfo_mob/src/app/tab2/tab2.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  @Input() rating: number ;
+
+
+
+  @Output() ratingChange: EventEmitter<number> = new EventEmitter();;
 
   constructor() {}
 
+  rate(index : number){
+    this.rating = index;
+    this.ratingChange.emit(this.rating);
+  }
+
+  isAboveRating(index : number): boolean{
+    return index > this.rating;
+  }
+
+  getColors(index : number){
+    if(this.isAboveRating(index)){
+      return COLORS.GREY;
+    }
+    switch (this.rating) {
+      case 1:
+      case 2:
+        return COLORS.RED;
+      case 3:
+        return COLORS.YELLOW;
+      case 4:
+      case 5:
+        return COLORS.GREEN;
+      default:
+        return COLORS.GREY;
+    }
+  }
+
+
+}
+
+enum COLORS {
+  GREY = "#E0E0E0",
+  GREEN = "#76FF03",
+  YELLOW = "FFCA28",
+  RED = "DD2C00"
 }
